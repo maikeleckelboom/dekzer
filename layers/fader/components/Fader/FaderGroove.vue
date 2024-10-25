715a39d26f9c0ef2dd7bedcbafba6ee925862a8f
maikeleckelboom <maikeleckelboom@outlook.com>
2024-10-25 04:43:07 +0200

    .

--- layers/fader/components/Fader/FaderGroove.vue
+++ layers/fader/components/Fader/FaderGroove.vue
@@ -4,53 +4,41 @@ import {
   injectFaderRootContext
 } from '~~/layers/fader/components/Fader/FaderRoot.vue'
 
-const { width, height, orientation, min, max, modelValue } = injectFaderRootContext({
-  orientation: shallowRef('horizontal'),
-  defaultValue: shallowRef(0),
-  modelValue: shallowRef(0),
-  offset: shallowRef(0),
-  width: shallowRef(0),
-  height: shallowRef(0),
-  min: shallowRef(0),
-  max: shallowRef(1)
-} as FaderContext)
+const { width, height, orientation, min, max, modelValue } =
+  injectFaderRootContext({
+    orientation: shallowRef('horizontal'),
+    defaultValue: shallowRef(0),
+    modelValue: shallowRef(0),
+    offset: shallowRef(0),
+    width: shallowRef(0),
+    height: shallowRef(0),
+    min: shallowRef(0),
+    max: shallowRef(1)
+  } as FaderContext)
 
 const isHorizontal = computed(() => orientation.value === 'horizontal')
 
-const containerSize = computed(() => (unref(isHorizontal) ? unref(width) : unref(height)))
-
-function getFillOffset(distanceCenter: number, fillSize: number) {
-  if (unref(isHorizontal)) {
-    return distanceCenter > 0 ? containerSize.value / 2 - fillSize : containerSize.value / 2
-  } else {
-    return distanceCenter > 0 ? containerSize.value / 2 : containerSize.value / 2 - fillSize
-  }
-}
-
-function getFillDimensions(
-  min: number,
-  max: number,
-  value: number,
-  totalSize: number
-): {
-  distance: number
-  size: number
-} {
-  const distance = Math.abs(min + (max - min) / 2 - value)
-  const size = (distance / (max - min)) * totalSize
-  return { distance, size }
-}
+const containerSize = computed(() => (unref(isHorizontal) ? width.value : height.value))
 
 const fillStyle = computed(() => {
-  const { distance, size } = getFillDimensions(
-    unref(min),
-    unref(max),
-    unref(modelValue),
-    unref(containerSize)
-  )
-  const fillOffset = getFillOffset(distance, size)
+  const distanceCenter = min.value + (max.value - min.value) / 2 - modelValue.value
+  const absDistanceCenter = Math.abs(distanceCenter)
+
+  // Calculate the fill size based on the distance from the center
+  const fillSize = (absDistanceCenter / (max.value - min.value)) * containerSize.value
+
+  // For horizontal, reverse the direction of fillOffset
+  const fillOffset = unref(isHorizontal)
+    ? distanceCenter > 0
+      ? containerSize.value / 2 - fillSize
+      : containerSize.value / 2
+    : distanceCenter > 0
+      ? containerSize.value / 2
+      : containerSize.value / 2 - fillSize
+
+  // Return the appropriate styles for horizontal and vertical sliders
   return {
-    [unref(isHorizontal) ? 'width' : 'height']: `${size}px`,
+    [unref(isHorizontal) ? 'width' : 'height']: `${fillSize}px`,
     [unref(isHorizontal) ? 'left' : 'top']: `${fillOffset}px`,
     [unref(isHorizontal) ? 'top' : 'left']: '0%',
     [unref(isHorizontal) ? 'height' : 'width']: '100%'
